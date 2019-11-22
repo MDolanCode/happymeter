dc1995832822949fe0b160a61b788440e7061045
Matt Dolan <mdolan.code@gmail.com>
2019-11-22 15:54:56 -0800

    Initial Commit

---
+++ HappyMeter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HappyMeter
+//
+//  Created by Papa Kuma on 2019-11-22.
+//  Copyright © 2019 Matt Dolan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
