d9da6ffb96ae6402af447a8add14446b36f6b749
Matt Dolan <mdolan.code@gmail.com>
2019-11-24 17:46:18 -0800

    Basic UI with constraints, programmed button, created image array, created random battery image when button pressed.

--- HappyMeter/ViewController.swift
+++ HappyMeter/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let batteryArray = ["battery1", "battery2", "battery3", "battery4","battery5"]
+    
+    var randomBattery: Int = 0
 
+    @IBOutlet weak var batteryImageView: UIImageView!
+    @IBOutlet weak var happyButton: UIButton!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+         happyButton.backgroundColor = UIColor.init(red: 0/255, green: 0/255, blue: 0/255, alpha: 1)
+         happyButton.layer.cornerRadius = 25.0
+         happyButton.tintColor = UIColor.white
+        
+         updateBatteryImage()
+        
     }
-
-
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        updateBatteryImage()
+        
+    }
+    
+    func updateBatteryImage() {
+        
+        randomBattery = Int.random(in: 0 ... 4)
+        batteryImageView.image = UIImage(named: batteryArray[randomBattery])
+        
+    }
+    
 }
 
